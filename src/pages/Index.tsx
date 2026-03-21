e90828c22cbed545e90a08ba766234f25534cc3a
vikingtrader47-lab <vikingtrader47@gmail.com>
2026-03-21 17:23:10 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import StatsBar from '@/components/StatsBar';
 import ProductsSection from '@/components/ProductsSection';
+import PlanosSection from '@/components/PlanosSection';
 import CtaSection from '@/components/CtaSection';
 import Footer from '@/components/Footer';
 import ParticlesBackground from '@/components/ParticlesBackground';
